614f6160e10c6dd0133b489a77f89fac9977258b
Prayag Sangode <67312119+prayag-sangode@users.noreply.github.com>
2023-07-16 22:06:43 +0530

    Update simple-dag.py

--- simple-dag.py
+++ simple-dag.py
@@ -1,45 +1,31 @@
-
 import datetime
 
-from airflow import models
-
+import airflow
 from airflow.operators import bash_operator
-from airflow.operators import python_operator
 
+# If you are running Airflow in more than one time zone
+# see https://airflow.apache.org/docs/apache-airflow/stable/timezone.html
+# for best practices
+YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
-
-default_dag_args = {
-    # The start_date describes when a DAG is valid / can be run. Set this to a
-    # fixed point in time rather than dynamically, since it is evaluated every
-    # time a DAG is parsed. See:
-    # https://airflow.apache.org/faq.html#what-s-the-deal-with-start-date
-    "start_date": datetime.datetime(2018, 1, 1),
+default_args = {
+    "owner": "Composer Example",
+    "depends_on_past": False,
+    "email": [""],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": datetime.timedelta(minutes=5),
+    "start_date": YESTERDAY,
 }
 
-# Define a DAG (directed acyclic graph) of tasks.
-# Any task you create within the context manager is automatically added to the
-# DAG object.
-with models.DAG(
-    "composer_sample_simple_greeting",
+with airflow.DAG(
+    "composer_sample_dag",
+    catchup=False,
+    default_args=default_args,
     schedule_interval=datetime.timedelta(days=1),
-    default_args=default_dag_args,
 ) as dag:
-    def greeting():
-        import logging
-
-        logging.info("Hello World!")
-
-    # An instance of an operator is called a task. In this case, the
-    # hello_python task calls the "greeting" Python function.
-    hello_python = python_operator.PythonOperator(
-        task_id="hello", python_callable=greeting
+    # Print the dag_run id from the Airflow logs
+    print_dag_run_conf = bash_operator.BashOperator(
+        task_id="print_dag_run_conf", bash_command="echo {{ dag_run.id }}"
     )
-
-    # Likewise, the goodbye_bash task calls a Bash script.
-    goodbye_bash = bash_operator.BashOperator(
-        task_id="bye", bash_command="echo Goodbye."
-    )
-
-    # Define the order in which the tasks complete by using the >> and <<
-    # operators. In this example, hello_python executes before goodbye_bash.
-    hello_python >> goodbye_bash
